48d9f511947b2b87fb1f553772111e9d4693fb5d
Nandana Satish <nandanapillai05@gmail.com>
2025-03-08 18:48:04 +0530

    haha

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/userModel');
 const { validateRegistration, validateLogin } = require('../middleware/validators');
 
 // Register a new user
